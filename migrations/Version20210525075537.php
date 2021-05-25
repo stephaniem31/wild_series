aa9409d95245822c516513644a624e63abfb1965
Stephanie Mariani <stephanie.mariani31@gmail.com>
2021-05-25 12:12:53 +0200

    quete 07

---
+++ migrations/Version20210525075537.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210525075537 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE program ADD program_id INT NOT NULL');
+        $this->addSql('ALTER TABLE program ADD CONSTRAINT FK_92ED77843EB8070A FOREIGN KEY (program_id) REFERENCES category (id)');
+        $this->addSql('CREATE INDEX IDX_92ED77843EB8070A ON program (program_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE program DROP FOREIGN KEY FK_92ED77843EB8070A');
+        $this->addSql('DROP INDEX IDX_92ED77843EB8070A ON program');
+        $this->addSql('ALTER TABLE program DROP program_id');
+    }
+}
---
+++ migrations/Version20210525100405.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210525100405 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE program DROP FOREIGN KEY FK_92ED77843EB8070A');
+        $this->addSql('DROP INDEX IDX_92ED77843EB8070A ON program');
+        $this->addSql('ALTER TABLE program CHANGE program_id category_id INT NOT NULL');
+        $this->addSql('ALTER TABLE program ADD CONSTRAINT FK_92ED778412469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('CREATE INDEX IDX_92ED778412469DE2 ON program (category_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE program DROP FOREIGN KEY FK_92ED778412469DE2');
+        $this->addSql('DROP INDEX IDX_92ED778412469DE2 ON program');
+        $this->addSql('ALTER TABLE program CHANGE category_id program_id INT NOT NULL');
+        $this->addSql('ALTER TABLE program ADD CONSTRAINT FK_92ED77843EB8070A FOREIGN KEY (program_id) REFERENCES category (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('CREATE INDEX IDX_92ED77843EB8070A ON program (program_id)');
+    }
+}
